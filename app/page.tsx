24835260856bc5c398c3ea0c5ab457059663c470
egidijus <145454567+egidijus1989@users.noreply.github.com>
2025-03-01 11:38:12 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Home page</h1>
+      <Button variant="default" size="default">
+        Click me
+      </Button>
+    </div>
+  );
+};
+export default HomePage;
